1852f20b9d0357589ed963a801a0a4eb2243007d
Srivatsa S <srivatsashankaran@gmail.com>
2017-07-14 01:12:10 +0530

    added the login_user_success in the AuthReducers and making the loginuser more robust

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,6 @@
 import { EMAIL_CHANGED } from './types';
 import { PASSWORD_CHANGED } from './types';
+import { LOGIN_USER_SUCCESS } from './types';
 import firebase from 'firebase';
 
 export const emailChanged = (text) => {
@@ -21,7 +22,7 @@ export const loginUser = ({ email, password }) => {
       //just to sign in a user
     firebase.auth().signInWithEmailAndPassword(email, password)
       .then(user => {
-        dispatch({ type: 'LOGIN_USER_SUCCESS', payload: user });
+        dispatch({ type: LOGIN_USER_SUCCESS, payload: user });
       });
   };
 };
