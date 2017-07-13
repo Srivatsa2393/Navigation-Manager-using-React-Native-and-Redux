319217cb9054a5ed6a0335a04b950b0515d4ddc9
Srivatsa S <srivatsashankaran@gmail.com>
2017-07-13 20:10:28 +0530

    creating an immutable state.. this is done by passing a new object in the AuthReducer

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -5,7 +5,10 @@ const INITIAL_STATE = { email: '' };
 export default (state= INITIAL_STATE, action) => {
   switch (action.type){
     case EMAIL_CHANGED:
-      console.log('action!');
+      //console.log('action!');
+      //state.email = action.payload(this cannot be done)
+      //return state(this cannot be done)
+      return { ...state, email: action.payload };
     default:
       return state;
   }
