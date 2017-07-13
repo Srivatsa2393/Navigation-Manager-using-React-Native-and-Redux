970fb3e4fd36a5f897d867613b0ea93cbff60355
Srivatsa S <srivatsashankaran@gmail.com>
2017-07-13 14:56:17 +0530

    added the default reducer and connected firebase

--- src/App.js
+++ src/App.js
@@ -2,11 +2,25 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
+import reducers from './reducers';
 
 class App extends Component{
+  componentWillMount(){
+    // Initialize Firebase
+    const config = {
+      apiKey: "AIzaSyBwH4II-6FpWQRyeqfgA4nc42xNWv87AKo",
+      authDomain: "navigation-manager.firebaseapp.com",
+      databaseURL: "https://navigation-manager.firebaseio.com",
+      projectId: "navigation-manager",
+      storageBucket: "navigation-manager.appspot.com",
+      messagingSenderId: "11831616426"
+    };
+      firebase.initializeApp(config);
+  }
   render() {
     return(
-      <Provider store={createStore()}>
+      <Provider store={createStore(reducers)}>
         <View>
           <Text>
             Hello!
