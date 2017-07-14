298791f436cb240d1ef9803859329dd1783ca6c7
Srivatsa S <srivatsashankaran@gmail.com>
2017-07-14 19:31:30 +0530

    Styling the picker and overriding the style in cardsection

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Card, Input, CardSection, Button } from './common';
-import { Picker } from 'react-native';
+import { Picker, Text } from 'react-native';
 import { connect } from 'react-redux';
 import { employeeUpdate } from '../actions'
 
@@ -28,7 +28,8 @@ class EmployeeCreate extends Component{
         </CardSection>
 
 
-        <CardSection>
+        <CardSection style={{ flexDirection: 'column' }}>
+          <Text style={styles.pickerTextStyle}>Shift</Text>
           <Picker
             style={{ flex: 1 }}
             selectedValue={this.props.shift}
@@ -53,6 +54,13 @@ class EmployeeCreate extends Component{
     );
   }
 }
+
+const styles = {
+  pickerTextStyle: {
+    fontSize: 18,
+    paddingLeft: 20
+  }
+};
 
 const mapStateToProps = (state) => {
   const { name, phone, shift } = state.employeeForm;
