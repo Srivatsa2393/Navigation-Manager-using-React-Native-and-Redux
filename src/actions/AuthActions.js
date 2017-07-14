7e30bd9ab5834b5fe9c3335eaa6e9ef7d2c52b8a
Srivatsa S <srivatsashankaran@gmail.com>
2017-07-14 18:32:23 +0530

    Refactoring all the actions and adding the reducer with EMPLOYEE_UPDATE case and returning a new object by doing key interpolation and setting the initial state of name, phone, shift to a empty string

---
+++ src/actions/AuthActions.js
@@ -0,0 +1,71 @@
+import { EMAIL_CHANGED } from './types';
+import { PASSWORD_CHANGED } from './types';
+import { LOGIN_USER_SUCCESS } from './types';
+import { LOGIN_USER_FAIL } from './types';
+import { LOGIN_USER } from './types';
+import { Actions } from 'react-native-router-flux';
+import firebase from 'firebase';
+
+export const emailChanged = (text) => {
+  return{
+    type: EMAIL_CHANGED,
+    payload: text
+  };
+};
+
+export const passwordChanged = (text) => {
+  return{
+    type: PASSWORD_CHANGED,
+    payload: text
+  };
+};
+
+// export const loginUser = ({ email, password }) => {
+//   return (dispatch) => {
+//       //just to sign in a user
+//     firebase.auth().signInWithEmailAndPassword(email, password)
+//       .then(user => {
+//         dispatch({ type: LOGIN_USER_SUCCESS, payload: user });
+//       })
+//       .catch(() => {
+//         firebase.auth().createUserWithEmailAndPassword(email, password)
+//           .then(user => {
+//             dispatch({ type: LOGIN_USER_SUCCESS, payload: user })
+//           });
+//       });
+//   };
+// };
+//identical as above
+
+
+export const loginUser = ({ email, password }) => {
+  return (dispatch) => {
+    dispatch({ type: LOGIN_USER });
+
+    //just to sign in a user
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then(user => loginUserSuccess(dispatch, user))
+      .catch((error) => {
+        console.log(error);
+
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+          .then(user => loginUserSuccess(dispatch, user))
+          .catch(() => loginUserFail(dispatch));
+      });
+  };
+};
+
+const loginUserSuccess = (dispatch, user){
+  dispatch({
+    type: LOGIN_USER_SUCCESS,
+    payload: user
+  });
+
+  Action.main();
+};
+
+const loginUserFail = (dispatch){
+  dispatch({
+    type: LOGIN_USER_FAIL
+  });
+}
