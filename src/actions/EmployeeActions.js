f53a97345a6c992391f152b03f2399d00d331fb5
Srivatsa S <srivatsashankaran@gmail.com>
2017-07-15 15:43:34 +0530

    resetting form properties

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -1,5 +1,7 @@
 import firebase from 'firebase';
 import { EMPLOYEE_UPDATE } from './types';
+import { EMPLOYEE_CREATE } from './types';
+import { Actions } from 'react-native-router-flux';
 
 
 export const employeeUpdate = ({ prop, value }) => {
@@ -14,6 +16,14 @@ export const employeeCreate = ({ name, phone, shift }) => {
   //we need to get access to the currently authenticated user we need their userId
   const { currentUser } = firebase.auth();
 
-  firebase.database().ref(`/users/${currentUser.uid}/employees`)
-    .push({ name, phone, shift }); //new data is pushed
+  //just wrapping this in the redux thunk no dipatch method is called
+  return(dispatch) =>{
+    firebase.database().ref(`/users/${currentUser.uid}/employees`)
+      .push({ name, phone, shift }) //new data is pushed
+      .then(() =>{
+        dispatch({ type: EMPLOYEE_CREATE });
+        Actions.employeeList({ type: 'reset' })
+      });
+  };
+
 };
